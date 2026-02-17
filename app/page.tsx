4d3b7df0a54b372d16e5db9b48b14911b7faa31d
ddhanushd <dhanushdevaraj17@gmail.com>
2026-02-17 10:56:20 +0530

    feat: implement Google OAuth login with session handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+
+import { supabase } from "./lib/supabase";
+
+
+
 export default function Home() {
+  const handleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+  };
+
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold mb-6">
         Smart Bookmark App 🚀
       </h1>
+
+      <button
+        onClick={handleLogin}
+        className="px-6 py-3 bg-black text-white rounded-lg"
+      >
+        Sign in with Google
+      </button>
     </main>
   );
 }
